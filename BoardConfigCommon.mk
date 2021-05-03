9290428d74068e61a28db44f61e61d7224d110f6
Chenyang Zhong <zhongcy95@gmail.com>
2021-05-03 07:41:59 +0200

    sdm660-common: create and symlink a dummy /metadata/apex
    Google moved apex sessions directory from /data/apex/sessions to
    /metadata/apex/sessions after commit:
    
    "Move apex sessions directory to /metadata"
    https://android.googlesource.com/platform/system/apex/+/36cf4bbac6108898a531aacc102dcc489d791b37
    
    Devices with a mounted metadata partition will have the needed
    directories set up by system/core/rootdir/init.rc. Xiaomi devices
    on sm6125 do not have a metadata partition out of the box, so things
    like "Google Play system update" will fail to install the update.
    
    Therefore, create a dummy directory under /data/vendor/metadata_apex
    and symlink it to /metadata/apex.
    
    The reason why the old /data/apex/sessions directory is not used
    for the symlink is that apexd will call migrateSessionsDirIfNeeded()
    to recursively copy things from the old directory to the new one.
    Creating the symlink from /data/apex/sessions may result in
    unintended behaviors.
    
    Signed-off-by: OdSazib <odsazib@gmail.com>
    Signed-off-by: Aryan Arora <arora.aryan7@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -7,6 +7,9 @@
 COMMON_PATH := device/asus/sdm660-common
 BUILD_BROKEN_DUP_RULES := true
 
+# APEX image
+DEXPREOPT_GENERATE_APEX_IMAGE := true
+
 # Architecture
 TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-a
